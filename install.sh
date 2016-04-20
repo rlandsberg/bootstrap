37573cbbbc46070557be3bb31030d7b0aab337fa
Rich L <richard.p.landsberg@gmail.com>
2016-04-20 22:04:08 +1000

    First commit
    learn Bash
    
    Dotfiles
    
    Adding the dotfiles
    
    Symlink sh
    
    Still learning sh
    
    symlink improvements
    
    Adding config dir
    
    Progress on user creation.
    
    working install script
    
    Adding in Sublime settings sync
    
    Update to comply with Shellcheck

---
+++ install.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+#Start of our install
+
+#First we need our command line tools
+
+
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
+
+#Set up a standard user account
+echo "Set up your standard user account"
+
+echo "Enter your desired user name: "
+read -r USERNAME
+
+echo "Enter a full name for this user: "
+read -r FULLNAME
+
+echo "Enter a password for this user: "
+read -r -s PASSWORD
+
+# ====
+
+#  Setting up our Homebrew group
+
+echo "Creating a brew group to use Homebrew as a non-admin account"
+dseditgroup -o create -r "Group to use Brew as non-admin" brew
+
+# ====
+
+
+# Create a UID that is not currently in use
+echo "Creating an unused UID for new user..."
+
+if  $UID -ne 0 ; then echo "Please run $0 as root." && exit 1; fi
+
+# Find out the next available user ID
+MAXID=$(dscl . -list /Users UniqueID | awk '{print $2}' | sort -ug | tail -1)
+USERID=$((MAXID+1))
+
+
+# Create the user account by running dscl (normally you would have to do each of these commands one
+# by one in an obnoxious and time consuming way.
+echo "Creating necessary files..."
+
+dscl . -create /Users/"$USERNAME"
+dscl . -create /Users/"$USERNAME" UserShell /bin/bash
+dscl . -create /Users/"$USERNAME" RealName "$FULLNAME"
+dscl . -create /Users/"$USERNAME" UniqueID "$USERID"
+dscl . -create /Users/"$USERNAME" PrimaryGroupID 20
+dscl . -create /Users/"$USERNAME" NFSHomeDirectory /Users/"$USERNAME"
+dscl . -passwd /Users/"$USERNAME" "$PASSWORD"
+
+
+# Add user to any specified groups
+echo "Adding user to specified groups..."
+dseditgroup -o edit -t user -a "$USERNAME" staff
+dseditgroup -o edit -t user -a "$USERNAME" brew
+
+#Add our admin account to the group too
+dseditgroup -o edit -t user -a "$(whoami)" brew
+
+
+
+# Create the home directory
+echo "Creating home directory..."
+createhomedir -c 2>&1 | grep -v "shell-init"
+
+echo "Created user #$USERID: $USERNAME ($FULLNAME)"
+
+
+#Next we need to set up our directories
+dir=" /Users/$USERNAME/Workspace"
+mkdir -p "$dir"
+chown "$USERNAME" "$dir"
+
+
+
+#Install Homebrew as our admin account
+sudo -u "$SUDO_USER" ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Now let change our permissions so that our non-admin user can use Homebrew
+
+sudo chgrp -R brew /usr/local
+sudo chgrp -R brew /Library/Caches/Homebrew
+sudo chmod -R g+w /usr/local
+sudo chmod -R g+w /Library/Caches/Homebrew
+
+# and if you plan to use caskroom too:
+sudo mkdir -p /opt/homebrew-cask/Caskroom
+sudo chgrp -R brew /opt/homebrew-cask
+sudo chmod -R g+w /opt/homebrew-cask
+
+# let's pull down our dotfiles and scripts
+
+cd "$dir" || exit
+sudo -u "$USERNAME" git clone --recursive https://github.com/rlandsberg/bootstrap.git
+sudo -u "$USERNAME" cd bootstrap
+
+
+#cd bootstrap
+#bash bootstrap.sh
+
---
+++ symlink-dotfiles.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Most of this is from https://github.com/paulmillr/dotfiles/blob/master/symlink-dotfiles.sh
+
+dev="$HOME/Workspace"
+dotfiles="$dev/bootstrap/dotfiles"
+
+if [[ -d "$dotfiles" ]]; then
+  echo "Symlinking dotfiles from $dotfiles"
+else
+  echo "$dotfiles does not exist"
+  exit 1
+fi
+
+link() {
+  from="$1"
+  to="$2"
+  echo "Linking '$from' to '$to'"
+  rm -f "$to"
+  ln -s "$from" "$to"
+}
+
+for location in $dotfiles/.{zshrc,tmux.conf} ; do
+  file="${location##*/}"
+  file="${file%.sh}"
+  link "$location" "$HOME/$file"
+done
+
+echo 'Applying sublime config...'
+  st=$(pwd)/sublime/packages
+  as="$HOME/Library/Application Support/Sublime Text 3/Packages"
+  asprefs="$as/User/Preferences.sublime-settings"
+  if [[ -d "$as" ]]; then
+    for theme in $st/Theme*; do
+      cp -r $theme $as
+    done
+    rm $asprefs
+    cp -r $st/pm-themes $as
